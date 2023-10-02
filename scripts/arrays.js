601e0bea8b1c60083728ab08792a4eb7f19113eb
natia-abramidze <abramidzenatia25@gmail.com>
2023-10-02 17:24:17 +0200

    lesson2Homework

---
+++ scripts/arrays.js
@@ -0,0 +1,12 @@
+let  arrayOfNames = ["green", "tree", "22", "bag", "10.10", "house"];
+
+alert(arrayOfNames[0]);
+alert (arrayOfNames[arrayOfNames.length-1]);
+
+color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+o = ["th","st","nd","rd"];
+
+alert(`1${o[1]} choice is ${color[0]}.
+2${o[2]} choice is ${color[1]}.
+3${o[3]} choice is ${color[2]}`)
+
---
+++ scripts/objects.js
@@ -0,0 +1,12 @@
+let student = {
+    fisrtName: "Mate",
+    lastNAme: "Gotua",
+    age: 19,
+    location: "Tbilisi"
+};
+
+
+student['phoneNumber'] = '3748563874';
+delete student.age;
+student.location = "Batumi";
+
